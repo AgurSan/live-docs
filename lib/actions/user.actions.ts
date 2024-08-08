0acfefdae915e06024cb97b101dacd860b7f1869
AgurSan <clement.sananikone@gmail.com>
2024-08-08 10:04:49 +0200

    build: mention feature

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,8 @@
 
 import { clerkClient as createCLerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
+import { text } from "stream/consumers";
 
 export const getClerkUsers = async ({ userIds}: { userIds: string[]}) => {
     try {
@@ -20,6 +22,25 @@ export const getClerkUsers = async ({ userIds}: { userIds: string[]}) => {
         const sortedUsers = userIds.map((email) => users.find((user) => user.email === email));
 
         return parseStringify(sortedUsers);
+    } catch (error) {
+        console.log(`Error fetching users: ${error}`);
+    }
+}
+
+export const getDocumentUsers = async ({ roomId, currentUser, text}: { roomId: string; currentUser: string; text: string}) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+
+        const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser);
+        
+        if(text.length) {
+            const lowercaseText = text.toLowerCase();
+            const filteredUsers = users.filter((email: string) => email.toLowerCase().includes(lowercaseText));
+
+            return parseStringify(filteredUsers);
+        }
+
+        return parseStringify(users);
     } catch (error) {
         console.log(`Error fetching users: ${error}`);
     }
