6913db76b90810427239080f04c87adbafa993d6
AgurSan <clement.sananikone@gmail.com>
2024-08-13 15:57:30 +0200

    build: modal ended

--- components/Collaborator.tsx
+++ components/Collaborator.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import React, { useState } from "react";
+import UserTypeSelector from "./UseTypeSelector";
+import { Button } from "./ui/button";
 
 const Collaborator = ({
   roomId,
@@ -12,7 +14,7 @@ const Collaborator = ({
   const [loading, setLoading] = useState(false);
 
   const shareDocumentHandler = async (type: string) => {};
-  const rremoveCollaboratorHandler = async (email: string) => {};
+  const removeCollaboratorHandler = async (email: string) => {};
 
   return (
     <li className="flex items-center justify-between gap-2 py-3">
@@ -24,7 +26,36 @@ const Collaborator = ({
           height={36}
           className="rounded-full size-9"
         />
+        <div>
+          <p className="line-clamp-1 text-sm font-semibold leading-4 text-white">
+            {collaborator.name}
+            <span className="text-10-regular pl-2 text-blue-100">
+              {loading && "updating..."}
+            </span>
+          </p>
+          <p className="text-sm font-light text-blue-100">
+            {collaborator.email}
+          </p>
+        </div>
       </div>
+
+      {creatorId === collaborator.id ? (
+        <p className="text-sm text-blue-100">Owner</p>
+      ) : (
+        <div className="flex items-center">
+          <UserTypeSelector
+            userType={userType as UserType}
+            setUserType={setUserType || "viewer"}
+            onClickHandler={shareDocumentHandler}
+          />
+          <Button
+            type="button"
+            onClick={() => removeCollaboratorHandler(collaborator.email)}
+          >
+            Remove
+          </Button>
+        </div>
+      )}
     </li>
   );
 };
